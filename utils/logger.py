fab278e86ba5e9733df314685dbdb8e1b8086343
lordjirosama <lordjiro615@gmail.com>
2026-08-21 14:12:13 +0530

    Add files via upload

---
+++ utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+from config import LOG_LEVEL
+
+
+def setup_logger(name: str = "AutoNewsBot") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, LOG_LEVEL.upper(), logging.INFO))
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            logging.Formatter(
+                fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        )
+        logger.addHandler(handler)
+
+    return logger
+
+
+logger = setup_logger()
